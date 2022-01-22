b4b67370a5af9b195d6ea00cb733d2924caea74b
Anjuman Hasan <anju2084@gmail.com>
2022-01-22 13:24:59 +0530

    Gender selection page added

---
+++ app/src/main/java/com/example/youfit/Activities/LoginAndStrating/genderselection.java
@@ -0,0 +1,16 @@
+package com.example.youfit.Activities.LoginAndStrating;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.youfit.R;
+
+public class genderselection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genderselection);
+    }
+}
